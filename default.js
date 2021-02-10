6298a64ac9dbfed7906c8737db59ebb3e5e19b19
Briton-Westerhaus <Briton.Westerhaus@gmail.com>
2021-02-09 19:49:08 -0500

    Partial work
    Work on arrows. Some stuff was infinite looping. Maybe some fixes are in place.

--- default.js
+++ default.js
@@ -28,18 +28,64 @@ function init() {
     selectInput(x, y);
 }
 
+function left() {
+    let element;
+    let x = activeX, y = activeY;
+    while (!element) {
+        y--;
+        if (y < 0) {
+            y = 8;
+            x--;
+        }
+        element = document.getElementById(x + ':' + y);
+        if (!element && y <= 0 && x <= 0)
+            return;
+    }
+    selectInput(x, y);
+}
+
+function up() {
+
+}
+
+function right() {
+    if (activeY == 8 && activeX == 8)
+        return;
+    let element;
+    let x = activeX, y = activeY;
+    while (!element) {
+        y++;
+        if (y > 8) {
+            y = 0;
+            x++;
+        }
+        element = document.getElementById(x + ':' + y);
+        if (!element && y >= 8 && x >= 8)
+            return;
+    }
+    selectInput(x, y);
+}
+
+function down() {
+
+}
+
 document.onkeydown = function(evt) {
     switch(evt.keyCode) {
         case 37: //left arrow
+            left();
             break;
 
         case 38: //up arrow
+            up();
             break;
         
         case 39: //right arrow
+            right();
             break;
 
         case 40: //down arrow
+            down();
             break;
 
         default:
