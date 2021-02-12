a27747247ff0a539e981cd152001f63233fc8383
Briton-Westerhaus <Briton.Westerhaus@gmail.com>
2021-02-11 19:50:12 -0500

    Revert "Partial work"
    This reverts commit 6298a64ac9dbfed7906c8737db59ebb3e5e19b19.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<link rel="stylesheet" type="text/css" href="default.css" />
 		<script type="text/javascript" src="default.js"></script>
 	</head>
-	<body<?php if (isSet($_SESSION['guessgrid']) && $_POST['submit'] != "Get a new Sudoku!") echo ' onload="init();"'; ?>>
+	<body<?php if (isSet($_SESSION['guessgrid'])) echo ' onload="init();"'; ?>>
 		<div class="content">
 			<h1>Sudoku</h1>
 			<form action="index.php" method="post">
