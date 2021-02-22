27debbbe89d6fbdc9a891e15564a2163d1e2737c
Briton-Westerhaus <Briton.Westerhaus@gmail.com>
2021-02-21 19:17:05 -0500

    Formaqtting
    Just some spacing and capitalization.

--- sudoku.php
+++ sudoku.php
@@ -2,14 +2,14 @@
     function makeGrid() {
         unset($_SESSION['fullgrid']);
         $_SESSION['fullgrid'] = array();
-        for($i = 0; $i < 9; $i++)
+        for ($i = 0; $i < 9; $i++)
             $_SESSION['fullgrid'][$i] = array();
         recurse(0, 0);
         $_SESSION['guessgrid'] = array();
-        for($i = 0; $i < 9; $i++)
+        for ($i = 0; $i < 9; $i++)
             $_SESSION['guessgrid'][$i] = array();
-        for($i = 0; $i < 9; $i++){
-            for($j = 0; $j < 9; $j++){
+        for ($i = 0; $i < 9; $i++) {
+            for ($j = 0; $j < 9; $j++) {
                 $_SESSION['guessgrid'][$i][$j] = $_SESSION['fullgrid'][$i][$j];
             }
         }
@@ -17,38 +17,38 @@
     }
 
     function recurse($x, $y) {
-        $numberexcluded = 0;
+        $numberExcluded = 0;
         $allowed = array(1 => true, 2 => true, 3 => true, 4 => true, 5 => true, 6 => true, 7 => true, 8 => true, 9 => true);
         for ($k = 0; $k < 9; $k++) {
             if($allowed[$_SESSION['fullgrid'][$k][$y]] != false){
                 $allowed[$_SESSION['fullgrid'][$k][$y]] = false;
-                $numberexcluded++;
+                $numberExcluded++;
             }
             if($allowed[$_SESSION['fullgrid'][$x][$k]] != false){
                 $allowed[$_SESSION['fullgrid'][$x][$k]] = false;
-                $numberexcluded++;
+                $numberExcluded++;
             }
         }
-        if ($numberexcluded == 9)
+        if ($numberExcluded == 9)
             return array('boolean' => false, 'value' => 0);
         $xpos = 2 - ($x % 3);
         $ypos = 2 - ($y % 3);
         for ($k = -2; $k <= 0; $k++) {
             for ($l = -2; $l <= 0; $l++) {
-                if($allowed[$_SESSION['fullgrid'][$x + $xpos + $k][$y + $ypos + $l]] != false) {
+                if ($allowed[$_SESSION['fullgrid'][$x + $xpos + $k][$y + $ypos + $l]] != false) {
                     $allowed[$_SESSION['fullgrid'][$x + $xpos + $k][$y + $ypos + $l]] = false;
-                    $numberexcluded++;
+                    $numberExcluded++;
                 }
             }
         }
-        if ($numberexcluded == 9)
+        if ($numberExcluded == 9)
             return array('boolean' => false, 'value' => 0);
         for ($i = 1; $i < 10; $i++) {
             if ($allowed[$i] == true)
-                $whichnumbers[] = $i;
+                $whichNumbers[] = $i;
         }
         if ($x == 8 && $y == 8) {
-            $_SESSION['fullgrid'][$x][$y] = $whichnumbers[rand(0, count($whichnumbers) - 1)];
+            $_SESSION['fullgrid'][$x][$y] = $whichNumbers[rand(0, count($whichNumbers) - 1)];
             return array('boolean' => true, 'value' => $_SESSION['fullgrid'][$x][$y]);
         }
         $nexty = $y;
@@ -58,24 +58,24 @@
         } else {
             $nextx = $x + 1;
         }
-        $testnext = false;
-        while (!$testnext) {
-            $_SESSION['fullgrid'][$x][$y] = $whichnumbers[rand(0, count($whichnumbers) - 1)];
-            $testnext = recurse($nextx, $nexty);
-            if ($testnext['value'] == 0) {
+        $testNext = false;
+        while (!$testNext) {
+            $_SESSION['fullgrid'][$x][$y] = $whichNumbers[rand(0, count($whichNumbers) - 1)];
+            $testNext = recurse($nextx, $nexty);
+            if ($testNext['value'] == 0) {
                 $allowed[$_SESSION['fullgrid'][$x][$y]] = false;
-                $numberexcluded++;
-                if ($numberexcluded == 9) {
+                $numberExcluded++;
+                if ($numberExcluded == 9) {
                     unset($_SESSION['fullgrid'][$x][$y]);
                     return array('boolean' => false, 'value' => 0);
                 }
-                unset($whichnumbers);
+                unset($whichNumbers);
                 for ($i = 1; $i < 10; $i++) {
                     if ($allowed[$i] == true)
-                        $whichnumbers[] = $i;
+                        $whichNumbers[] = $i;
                 }
             }
-            $testnext = $testnext['boolean'];
+            $testNext = $testNext['boolean'];
         }
         return array('boolean' =>true, 'value' => $_SESSION['fullgrid'][$x][$y]);
     }
